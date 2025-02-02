8aab949d18f6d186cd3221a644f46be020b0db8e
Amir Javeed <amir.javed@cloudcardinc.com>
2025-02-02 07:46:12 +0500

    Add coming soon

--- src/app/player-list/player-list.component.ts
+++ src/app/player-list/player-list.component.ts
@@ -5,11 +5,12 @@ import { JerseyDirective } from '../../Shared/directives/jersey.directive';
 import { TagModule } from 'primeng/tag';
 import { SkeletonModule } from 'primeng/skeleton';
 import {SinglePlayerDetailComponent} from '../single-player-detail/single-player-detail.component'
+import { ComingSoonComponent } from "../projections/coming-soon/coming-soon.component";
 
 @Component({
   selector: 'app-player-list',
   standalone: true,
-  imports: [HeaderComponent , JerseyDirective , TagModule , SkeletonModule , SinglePlayerDetailComponent],
+  imports: [HeaderComponent, JerseyDirective, TagModule, SkeletonModule, SinglePlayerDetailComponent, ComingSoonComponent],
   templateUrl: './player-list.component.html',
   styleUrl: './player-list.component.scss'
 })
@@ -18,6 +19,7 @@ export class PlayerListComponent implements OnInit {
   selectedGame: string = 'nba';
   playerList: any[] = [];
   playerListLoader: boolean = false;
+  showComingSoon: boolean = false;
 
   seletedPlayer: number =0;
 
@@ -27,17 +29,20 @@ export class PlayerListComponent implements OnInit {
   }
 
   onGameChange(event: any) {
+    this.showComingSoon = false;
     if(event){
       this.selectedGame = event;
       this.getPlayersList();
       return;
     }
 
+    this.showComingSoon = true;
+
   }
 
   getPlayersList() {
     this.playerListLoader = true;
-    this.sharedS.sendGetRequest(`nba/players/list`).subscribe({
+    this.sharedS.sendGetRequest(`${this.selectedGame}/players/list`).subscribe({
       next: (res: any) => {
         this.playerListLoader = false;
         if (res.status === 200) {
