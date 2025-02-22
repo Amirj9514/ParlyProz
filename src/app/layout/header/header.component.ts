18bc304fd30db5dccc786dec15a767ca6be23b98
Amir Javeed <amir.javed@cloudcardinc.com>
2025-02-22 16:29:35 +0500

    sidebar

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { SvgIconDirective } from '../../../Shared/directives/svg-icon.directive';
-
+import { Drawer, DrawerModule } from 'primeng/drawer';
+import { ButtonModule } from 'primeng/button';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [SvgIconDirective],
+  imports: [SvgIconDirective ,DrawerModule ,ButtonModule , RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  visible = false;
+  @ViewChild('drawerRef') drawerRef!: Drawer;
+
+  closeCallback(e:any): void {
+      this.drawerRef.close(e);
+  }
 
 }
