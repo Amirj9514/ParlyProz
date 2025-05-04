c52e932304f28b80f6796e13e2683ed8edb6dd38
Amir Javeed <amir.javed@cloudcardinc.com>
2025-05-04 12:02:43 +0500

    changes envirment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
-    apiUrl: 'https://sportsdataapi-frankfurt-region.onrender.com/',
+    // Production environment url
+    apiUrl: 'https://sportsdataapi-5l8y.onrender.com/',
+
+    // local development
     // apiUrl: 'https://sportsdataapi.onrender.com/',
 };
