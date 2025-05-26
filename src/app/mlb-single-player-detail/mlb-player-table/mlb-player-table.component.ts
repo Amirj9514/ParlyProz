5f67d7ca70cd800a48118f8d5d29dbd5afcc827e
Amir Javeed <amir.javed@cloudcardinc.com>
2025-05-26 22:58:45 +0500

    remove null value form data

--- src/app/mlb-single-player-detail/mlb-player-table/mlb-player-table.component.ts
+++ src/app/mlb-single-player-detail/mlb-player-table/mlb-player-table.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { PlayerStatsService } from '../../../Shared/services/player-stats.service';
-import { NhlService } from '../../../Shared/services/nhl.service';
 import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
 import { MlbService } from '../../../Shared/services/mlb.service';
@@ -8,24 +6,48 @@ import { MlbService } from '../../../Shared/services/mlb.service';
 @Component({
   selector: 'app-mlb-player-table',
   standalone: true,
-    imports: [TableModule , CommonModule],
+  imports: [TableModule, CommonModule],
   templateUrl: './mlb-player-table.component.html',
-  styleUrl: './mlb-player-table.component.scss'
+  styleUrl: './mlb-player-table.component.scss',
 })
 export class MlbPlayerTableComponent {
- players: any[] = [];
-  constructor(private  nhlService:MlbService){}
+  players: any[] = [];
+  statsList: any[] = [];
+  constructor(private nhlService: MlbService) {}
 
   ngOnInit(): void {
     this.getPlayerList();
   }
 
   getPlayerList() {
-    this.players =  this.nhlService.getPlayerData(15 , 'desc');
+    this.players = this.nhlService.getPlayerData(15, 'desc');
+
+    if(!this.players.length) return;
+    const statsList = this.nhlService.getStatsList();
+    let statsKey:string = '';
+    let newStatsList: any[] = [];
+
+    statsList.map((stat) => {
+      if (stat.id !== 'H+R+RBI') {
+        const statName = this.nhlService.getStatsKeyByStatsId(stat.id);
+        let isStatFound = false;
+        for (const player of this.players) {
+          if (player[statName.key] || player[statName.key] === 0) {
+            statsKey = statName.key;
+            isStatFound = true;
+            break;
+          }
+        }
+
+        if (isStatFound) {
+          newStatsList.push({name:stat.id , key: statsKey});
+        }
+      }
+    });
+    this.statsList = newStatsList;
   }
 
   roundValue(value: number) {
-    return (value);
+    return value;
   }
-
 }
