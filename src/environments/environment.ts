7101e97613a1dc38f6c891b438002ccdfeaba6b3
Amir Javeed <amir.javed@cloudcardinc.com>
2025-11-19 23:03:03 +0500

    env cahnge

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     // Production environment url
-    apiUrl: 'https://sportsdataapi-5l8y.onrender.com/',
+    // apiUrl: 'https://sportsdataapi-5l8y.onrender.com/',
 
     // local development
-    // apiUrl: 'https://sportsdataapi.onrender.com/',
+    apiUrl: 'https://sportsdataapi.onrender.com/',
 };
